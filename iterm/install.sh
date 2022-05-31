49b524166a2085a3d29e5e16c64721bb932fbce7
Philip Downer <pdowner@eezy.com>
2022-05-31 17:14:04 -0600

    Adds configuration profile for iTerm.

---
+++ iterm/install.sh
@@ -0,0 +1,8 @@
+local ITERM_PROFILE_DIR="${HOME}/Library/Application\ Support/iTerm2/DynamicProfiles"
+local ITERM_PROFILE_FILENAME="eezy-default-iterm2-profile.json" 
+
+mkdir -p $ITERM_PROFILE_DIR
+
+if [ ! -r "${ITERM_PROFILE_DIR}/${ITERM_PROFILE_FILENAME}" ]; then
+    cp "${DOTFILES}/iterm/${ITERM_PROFILE_FILENAME} "${ITERM_PROFILE_DIR}/${ITERM_PROFILE_FILENAME}
+fi
