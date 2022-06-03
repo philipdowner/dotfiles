5f2a0fd9bfefe576f096defa5572dcde90f98d69
Philip Downer <pdowner@eezy.com>
2022-06-03 16:37:01 -0600

    Adds VSCode extensions during install. Sets up global EditorConfig file.

---
+++ vscode/install.sh
@@ -0,0 +1,6 @@
+code --install-extension esbenp.prettier-vscode
+code --install-extension EditorConfig.EditorConfig
+code --install-extension Shelex.vscode-cy-helper
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension eamodio.gitlens
+code --install-extension streetsidesoftware.code-spell-checker
