75439fd7de5da3bfbb54c7244e6679d1e6566c96
Philip Downer <pdowner@eezy.com>
2022-06-03 16:32:10 -0600

    Reset press-and-hold on keyboards.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -28,8 +28,8 @@ defaults write com.apple.screencapture disable-shadow -bool true
 ###############################################################################
 # Input Devices                                                               #
 ###############################################################################
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
+# Enable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool true #Setting 'false' conflicts with several apps
 
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 1
