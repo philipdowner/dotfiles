9f79efcf8998897949b813a3777c8bbdfbe86d46
Philip Downer <pdowner@eezy.com>
2022-06-06 08:05:15 -0600

    Adds 'repo' alias for GH CLI

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,3 +6,6 @@ alias gb='git branch'
 alias gs='git s' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+
+#Github CLI Aliases
+alias repo='gh repo view -w'
