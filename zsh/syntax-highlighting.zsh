f3a2832490a40037126c39723894cb8cfe4dfda9
Philip Downer <manifestphil@gmail.com>
2026-04-06 20:12:11 -0600

    Fixes ZSH syntax highlighting error

--- zsh/syntax-highlighting.zsh
+++ zsh/syntax-highlighting.zsh
@@ -1,2 +1,5 @@
-# https://formulae.brew.sh/formula/zsh-syntax-highlighting
-source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [[ -r /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif command -v brew >/dev/null 2>&1; then
+  source "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+fi
